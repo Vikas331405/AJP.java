4bf74d1c8379143282108d2b56f49e197387bd51
Vikas Kasani <vikaskasani@gmail.com>
2024-12-12 18:46:09 +0530

    Add files via upload

---
+++ Arithtematicopera.java
@@ -0,0 +1,24 @@
+package AJP.java.com;
+
+public class Arithtematicopera {
+	
+	public static void main(String []args) {
+	/*	
+		int a=10,b=17;
+		System.out.println("sum="+(+a+b));
+		System.out.println("difference="+(a-b));
+		System.out.println("product="+(a*b));
+		System.out.println("quotient="+(a/b));
+		System.out.println("reminder="+(a%b));
+		*/
+		
+		double a=55.0001;
+		double b=100.5769;
+		System.out.println("sum="+(+a+b));
+		System.out.println("difference="+(a-b));
+		System.out.println("product="+(a*b));
+		System.out.println("quotient="+(a/b));
+		System.out.println("reminder="+(a%b));
+	}
+
+}
